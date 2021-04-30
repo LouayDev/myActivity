c8d120a34863b3e1a7a0d6fd4e1d23308082ba4e
louai28 <amrou.123.gg@gmail.com>
2021-04-30 23:40:40 +0100

    completeing the base setup and adding the rest of the pages

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const express = require("express");
 const session = require("express-session");
 const bcrypt = require("bcrypt");
 const LocalStrategy = require("passport-local").Strategy;
+const MongoStore = require("connect-mongo")(session);
+const expressLayouts = require("express-ejs-layouts");
 
-//general setup up the server --------------//
+//general setup  --------------//
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("server running on :", "http://localhost:5000"));
@@ -15,4 +17,14 @@ app.listen(PORT, console.log("server running on :", "http://localhost:5000"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //using the ejs
+app.use(expressLayouts);
 app.set("view-engine", "ejs");
+//setting a static folder
+app.use(express.static("./public"));
+
+// the routes -------------------//
+app.use("/", require("./routes/register"));
+app.use("/login", require("./routes/login"));
+app.use("/forgot", require("./routes/forgot"));
+app.use("/reset_password", require("./routes/password_reset"));
+app.use("/dasboard", require("./routes/dashboard"));
--- database.js
+++ database.js
@@ -5,3 +5,15 @@ const connection = mongoose.createConnection(URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+const UsersSchema = new mongoose.Schema({
+  firstName: { type: string, trim: true, default: "" },
+  lastName: { type: string, trim: true, default: "" },
+  email: { type: string, trim: true, default: "" },
+  password: { type: string, trim: true, default: "" },
+  date: { type: Date, default: Date.now },
+});
+
+const Users = connection.model("Users", UsersSchema);
+
+module.exports = { Users };
